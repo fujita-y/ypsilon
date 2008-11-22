e4962897c0fafd42b0e08bdd8838af9e1f190533
y.fujita.lwp <y.fujita.lwp@9c42657c-9a4e-0410-8d72-adfee4fd9969>
2008-11-22 15:03:06 +0000

    fix (srfi :6)

--- trunk/sitelib/srfi/%3a6.scm
+++ trunk/sitelib/srfi/%3a6.scm
@@ -1,5 +1,5 @@
 #!nobacktrace
 
 (library (srfi :6)
-  (export (open-input-string open-output-string get-output-string))
+  (export open-input-string open-output-string get-output-string)
   (import (srfi srfi-6)))
