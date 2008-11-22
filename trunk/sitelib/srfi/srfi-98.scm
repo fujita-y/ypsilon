a4b0d31db80c71863c7af986c8081f8c0d0a6895
y.fujita.lwp <y.fujita.lwp@9c42657c-9a4e-0410-8d72-adfee4fd9969>
2008-11-22 12:49:48 +0000

    add srfi-98

---
+++ trunk/sitelib/srfi/srfi-98.scm
@@ -0,0 +1,6 @@
+#!nobacktrace
+
+(library (srfi srfi-98)
+  (export (rename (lookup-process-environment get-environment-variable)
+                  (process-environment->alist get-environment-variables)))
+  (import (core primitives)))
