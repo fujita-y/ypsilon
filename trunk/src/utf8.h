cff06f7babbcb0ce5ee3f06fc5b40796ca28a1c0
y.fujita.lwp <y.fujita.lwp@9c42657c-9a4e-0410-8d72-adfee4fd9969>
2009-03-07 13:34:23 +0000

    fix utf-8 bad code detection

--- trunk/src/utf8.h
+++ trunk/src/utf8.h
@@ -17,7 +17,7 @@ class object_heap_t;
 
 int utf8_byte_count(const uint8_t datum);
 int cnvt_ucs4_to_utf8(uint32_t ucs4, uint8_t utf8[4]);
-int cnvt_utf8_to_ucs4(const uint8_t utf8[4], uint32_t* ucs4);
+int cnvt_utf8_to_ucs4(const uint8_t* utf8, uint32_t* ucs4);
 int utf8_char_index_to_byte_offset(const uint8_t datum[], int index, int limit);
 int utf8_string_ref(scm_string_t obj, int index);
 int utf8_string_length(scm_string_t obj);
