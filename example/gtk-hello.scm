2108bde5956d09de068869f65e6bab0359e57b38
y.fujita.lwp <y.fujita.lwp@9c42657c-9a4e-0410-8d72-adfee4fd9969>
2009-03-17 00:34:02 +0000

    update gtk-hello.scm

--- example/gtk-hello.scm
+++ example/gtk-hello.scm
@@ -11,51 +11,15 @@
 ;;   OpenBSD: libgtk-x11-2.0.so
 
 (import (rnrs)
-        (srfi :28)
-        (ypsilon ffi))
-
-; minimal bindings for GTK hello world
-
-(define libgtk-name (cond (on-linux   "libgtk-x11-2.0.so.0")
-                          (on-freebsd "libgtk-x11-2.0.so")
-                          (on-openbsd "libgtk-x11-2.0.so")
-                          (on-darwin  "Gtk.framework/Gtk")
-                          (else
-                           (assertion-violation #f "can not locate GTK library, unknown operating system"))))
-
-(define libgtk (load-shared-object libgtk-name))
-
-(define-syntax define-function
-  (syntax-rules ()
-    ((_ ret name args)
-     (define name (c-function libgtk libgtk-name ret name args)))))
-
-(define-function void  gtk_init ([int] (* [char*])))
-(define-function void* gtk_window_new (int))
-(define-function void  gtk_window_set_title (void* char*))
-(define-function void  gtk_window_resize (void* int int))
-(define-function void* gtk_button_new_with_label (char*))
-(define-function void  gtk_widget_show (void*))
-(define-function void  gtk_widget_destroy (void*))
-(define-function void  gtk_main ())
-(define-function void  gtk_main_quit ())
-(define-function void  gtk_container_add (void* void*))
-(define-function void  gtk_container_set_border_width (void* int))
-(define-function int   g_signal_connect_data (void* char* [c-callback int (void* void* void*)] void* void* int))
-(define-function int   g_signal_connect_object (void* char* [c-callback int (void*)] void* int))
-
-(define GTK_WINDOW_TOPLEVEL 0)
-(define G_CONNECT_SWAPPED   2)
-
-(define g_signal_connect
-  (lambda (instance detailed_signal c_handler data)
-    (g_signal_connect_data instance detailed_signal c_handler data 0 0)))
-
-(define g_signal_connect_swapped
-  (lambda (instance detailed_signal c_handler data)
-      (g_signal_connect_object instance detailed_signal c_handler data G_CONNECT_SWAPPED)))
-
-; GTK hello world
+        (srfi :48)
+        (ypsilon gtk constants)
+        (ypsilon gtk init)
+        (ypsilon gtk main)
+        (ypsilon gtk window)
+        (ypsilon gtk container)
+        (ypsilon gtk button)
+        (ypsilon gtk widget)
+        (ypsilon gobject signal))
 
 (gtk_init (vector (length (command-line))) (apply vector (command-line)))
 (let ((window (gtk_window_new GTK_WINDOW_TOPLEVEL))
