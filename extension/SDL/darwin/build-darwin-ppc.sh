61480f89508d25d72c40e7bca27ea63908de448d
y.fujita.lwp <y.fujita.lwp@9c42657c-9a4e-0410-8d72-adfee4fd9969>
2009-05-09 06:58:56 +0000

    add gtk-cairo-demo.scm

--- extension/SDL/darwin/build-darwin-ppc.sh
+++ extension/SDL/darwin/build-darwin-ppc.sh
@@ -1,1 +1,4 @@
-/usr/bin/gcc-4.0 -x objective-c -arch ppc -Os -fvisibility=hidden -I/Library/Frameworks/SDL.framework/Headers -c SDLMain.m -o ppc/SDLMain.o
+/usr/bin/gcc-4.0 -x objective-c -arch ppc -Os -fvisibility=hidden \
+-I/Library/Frameworks/SDL.framework/Headers \
+-I/System/Library/Frameworks/SDL.framework/Headers \
+-c SDLMain.m -o ppc/SDLMain.o
