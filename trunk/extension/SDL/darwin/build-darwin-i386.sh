148734e138ef2d172191f604926415f33cb8dc09
y.fujita.lwp <y.fujita.lwp@9c42657c-9a4e-0410-8d72-adfee4fd9969>
2009-06-09 07:44:35 +0000

    update setup-win32.vdproj

--- trunk/extension/SDL/darwin/build-darwin-i386.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/gcc-4.0 -x objective-c -arch i386 -Os -fvisibility=hidden \
--I/Library/Frameworks/SDL.framework/Headers \
--I/System/Library/Frameworks/SDL.framework/Headers \
--c SDLMain.m -o i386/SDLMain.o
